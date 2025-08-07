05b65884a49a2a1d6358b90fdacc6a4737ef31fd
Charles Wilson <charles.wilson@umontreal.ca>
2025-08-07 14:50:22 -0400

    added folder structure to jet_on_wall

--- jet_on_wall/slurm_scripts/jet_on_wall_job.sh
+++ jet_on_wall/slurm_scripts/jet_on_wall_job.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-#SBATCH --time=0-02:00
-#SBATCH --account=def-blaisbru
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
+#SBATCH --time=1-00:00
+#SBATCH --account=rrg-blaisbru
+#SBATCH --nodes=2
+#SBATCH --ntasks-per-node=192
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=48G
+#SBATCH --mem=249G
 
 export OMP_NUM_THREADS=1
 ulimit -s 8192
 
 source $HOME/.bashrc
 source $HOME/.dealii
-srun $HOME/lethe/inst/bin/lethe-fluid ../jet_on_wall_mini.prm
+srun $HOME/lethe/inst/bin/lethe-fluid $GRAPHITE_PATH/jet_on_wall/prm/jet_on_wall.prm
