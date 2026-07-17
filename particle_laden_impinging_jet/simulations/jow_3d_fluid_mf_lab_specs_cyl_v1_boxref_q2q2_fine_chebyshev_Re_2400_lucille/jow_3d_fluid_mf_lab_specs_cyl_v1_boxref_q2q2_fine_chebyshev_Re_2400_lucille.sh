154ee9b22e9cc1f12abf6555651fed9221074327
Charles Wilson <charles.wilson@etud.polymtl.ca>
2026-07-17 16:50:52 -0400

    improved cylinder mesh, new Re_ series using it

---
+++ particle_laden_impinging_jet/simulations/jow_3d_fluid_mf_lab_specs_cyl_v1_boxref_q2q2_fine_chebyshev_Re_2400_lucille/jow_3d_fluid_mf_lab_specs_cyl_v1_boxref_q2q2_fine_chebyshev_Re_2400_lucille.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --job-name=charles-jow_3d_fluid_mf_lab_specs_cyl_v1_boxref_q2q2_fine_chebyshev_Re_2400_lucille
+#SBATCH --output=log.txt
+#SBATCH --ntasks=128
+#SBATCH --time=24:00:00
+
+
+source ~/.job_launch
+source ~/.lethe
+mpirun -np 128 lethe-fluid-matrix-free /data/charles/graphite_project/particle_laden_impinging_jet/simulations/jow_3d_fluid_mf_lab_specs_cyl_v1_boxref_q2q2_fine_chebyshev_Re_2400_lucille/jow_3d_fluid_mf_lab_specs_cyl_v1_boxref_q2q2_fine_chebyshev_Re_2400_lucille.prm
+source ~/.job_stop
---
+++ particle_laden_impinging_jet/simulations/jow_3d_fluid_mf_lab_specs_cyl_v1_boxref_q2q2_fine_chebyshev_Re_480_fir/jow_3d_fluid_mf_lab_specs_cyl_v1_boxref_q2q2_fine_chebyshev_Re_480_fir.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --time=0-01:00
+#SBATCH --account=def-blaisbru
+#SBATCH --nodes=4
+#SBATCH --ntasks-per-node=192
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=200G
+#SBATCH --mail-user=charles.wilson@etud.polymtl.ca
+#SBATCH --mail-type=ALL
+#SBATCH --job-name=jow_3d_fluid_mf_lab_specs_cyl_v1_boxref_q2q2_fine_chebyshev_Re_480_fir
+
+export OMP_NUM_THREADS=1
+ulimit -s 8192
+
+export OMPI_MCA_btl_openib_warn_no_device_params_found=0
+
+export PLIJ_PATH=$SCRATCH/graphite_project/particle_laden_impinging_jet
+source $HOME/.dealii
+
+# prm paths are relative to this directory
+cd $PLIJ_PATH/simulations/jow_3d_fluid_mf_lab_specs_cyl_v1_boxref_q2q2_fine_chebyshev_Re_480_fir || exit 1
+
+srun $HOME/lethe/inst/bin/lethe-fluid-matrix-free ./jow_3d_fluid_mf_lab_specs_cyl_v1_boxref_q2q2_fine_chebyshev_Re_480_fir.prm
